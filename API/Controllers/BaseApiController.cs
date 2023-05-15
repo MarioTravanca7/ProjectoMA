c3cf9d6d1573549e81d6919711780a445f495930
MarioTravanca7 <mtravanca92@gmail.com>
2023-05-15 14:35:12 +0000

    EoS4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,17 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-
 namespace API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
+         private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= 
+            HttpContext.RequestServices.GetService<IMediator>();
     }
 }
